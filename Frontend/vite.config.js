263b4a5185faf78af1b5ab0a63c7735a4e32ea80
Kushal Bansal <321kushalbansal.kb@gmail.com>
2025-01-16 01:56:14 +0530

    stage 9

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,6 +6,9 @@ import checker from 'vite-plugin-checker';
 // ----------------------------------------------------------------------
 
 export default defineConfig({
+  build: {
+    target: 'es2022',  // Use ES2022 or a higher version to support top-level await
+  },
   plugins: [
     react(),
     checker({
